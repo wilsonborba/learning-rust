e5c026f44d480b3da0a35048427d01471b74a173
wilsonborba <wilsonmatheuslimaborba@gmail.com>
2025-03-15 12:17:39 +0700

    feat(calculator): app calculator finish with simple operations

---
+++ calculator/src/operations.rs
@@ -0,0 +1,37 @@
+
+
+
+#[allow(dead_code)]
+pub fn add(a: f64, b: f64) -> f64 {
+    a + b
+}
+
+#[allow(dead_code)]
+pub fn subtract(a: f64, b: f64) -> f64 {
+    a - b
+}
+
+
+#[allow(dead_code)]
+pub fn multiply(a: f64, b: f64) -> f64 {
+    a * b
+}
+
+#[allow(dead_code)]
+pub fn divide(a: f64, b: f64) -> f64 {
+    a / b
+}
+
+#[allow(dead_code)]
+pub fn modulo(a: f64, b: f64) -> f64 {
+    a % b
+}
+
+#[allow(dead_code)]
+pub fn power(a: f64, b: f64) -> f64 {
+    a.powf(b)
+}
+
+
+
+
